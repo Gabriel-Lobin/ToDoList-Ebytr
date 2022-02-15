e1d0e85f2a9f4b97ebea44076c8efb74ba2ce449
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2022-02-15 16:17:59 -0300

    [scripts] start e dev

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`servidor rodando na PORTa:${port}!`));
+app.listen(PORT, () => console.log(`servidor rodando na porta:${PORT}!`));
